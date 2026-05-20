238d3527faa16b4d64d81a8b5fea11f86a70dc97
Yestagit <medyayesta@gmail.com>
2026-05-20 14:45:07 +0700

    Initial commit: iniversi vercel ke-1

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Halo, Ini Project Vercel SDT B!</h1>
+      <h2 className="text-4xl font-italic">Selamat Datang di web service</h2>
       <p className="mt-4">Berhasil di-deploy lewat Git.</p>
     </main>
   );
